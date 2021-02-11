cce4c23bc7b5ecf9209bebe112706b80de0a1253
Shuji Miyamoto <smiyamoto@MacBook-Pro.local>
2021-02-10 17:18:19 -0800

    Section 7-30

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 # use a django helper model instead of directly importing it for scalability
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user, get_user_model
+
 
 class ModelTests(TestCase):
 
@@ -14,4 +15,27 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(user.email, email)
+        # need check_password fucntion as it's encrypted
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@LONDONAPPDEV.com'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@londonappdev.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
